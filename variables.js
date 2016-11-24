8217ad6eefeedb54778dfe41c42c046bd127c164
ZachMayer <GenericGuy35@gmail.com>
2016-11-23 17:43:51 -0800

    Quick change so I can work remotely
    Using IP routing means I don't have to go in and mess with host files
    when I'm without a network connection. It's lame but it works for now.
    I'll do something with it later.

--- variables.js
+++ variables.js
@@ -25,7 +25,7 @@ var WEBPACK_DEV_SERVER_PORT = 3000;
 // process.env object contains environment variables passed to the node.js process.
 // For example, you can see NODE_ENV passed to node in the "scripts" section of package.json
 if (process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'production') {
-    SERVER_HOST = 'localhost';
+    SERVER_HOST = '127.0.0.1'; // yes I know. But this works when disconnected from a network without changing system settings or host files.
     SERVER_PORT = process.env.PORT || 8080;
 } else {
     var errorText = '[' + path.basename(__filename) + '] ERROR: NODE_ENV is not set: ' + process.env.NODE_ENV;
