173d3754c08f128a7bbad4029f4d2547003862ec
Zachary Mayer <zach.s.mayer@gmail.com>
2017-01-26 16:42:31 -0800

    Cleaning up the loading screen and added a github thinger.

--- Web/web/server/views/app.jsx
+++ Web/web/server/views/app.jsx
@@ -2,6 +2,7 @@
 
 import getWebpackAssets from '../get-webpack-assets';
 import React from 'react';
+import GithubCorner from 'react-github-corner';
 
 var App = React.createClass({
     render: function () {
@@ -10,10 +11,24 @@ var App = React.createClass({
                 <head>
                     <meta charSet='utf-8' />
                     <meta name='viewport' content='width=device-width, initial-scale=1' />
-                    <title>Radioscope</title>                    
+                    <title>Radioscope</title>
+                    <link rel='stylesheet' href='/assets/loader.css' />
                 </head>
-                <body>                
-                    <div id='appContainer' className='flex-container container'>This text will be replaced by an app component</div>                    
+                <body>    
+                <GithubCorner
+                    href={'https://github.com/ZachMayer35/Radioscope'}
+                    bannerColor="#337ab7"
+                    octoColor="#fff"
+                    width={80}
+                    height={80}
+                    direction="right"
+                    />
+                <div id='appContainer' className='flex-container container'>
+                    <div style={{ height: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                        <div style={{ resize: 'both' }} className='loader'>
+                        </div>
+                    </div>
+                </div>                    
                 </body>
                 <script async src={getWebpackAssets().app.js}></script>
             </html>
